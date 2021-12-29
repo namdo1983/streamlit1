14250978bb58e213be2384d420d4299427fdfe0c
NamAtlantech <nam.do@creqavn.net>
2021-12-29 11:36:19 +0700

    changed err

--- demo1.py
+++ demo1.py
@@ -3,28 +3,31 @@ import requests
 
 s = requests.Session()
 
+
 def layout():
     st.write('Check Access Domain...')
     uploaded_file = st.file_uploader('Browser A File', type=['txt'])
-    print(dir(uploaded_file))
+    # print(dir(uploaded_file))
     if st.button('Process') and uploaded_file is not None:
         # To read file as bytes:
         data = uploaded_file.readlines()
-        st.write(data)
+        # st.write(data)
         # st.text(data)
 
-        for item in data:
+        for idx, item in enumerate(data, start=1):
             try:
                 r = s.get(item.strip(), timeout=5)
             except Exception as e:
                 print(e)
-                st.write(str(e))
+                err = {f'{idx}. {item.decode("utf-8").strip()}': str(e)}
+                # err = f'{idx}. {item.decode("utf-8").strip()}' + str(e)
+                st.write(err)
             else:
                 if r.ok:
                     print('OK')
                 else:
                     print(item, r.status_code, r.reason)
-                st.write(r.url, r.status_code, r.reason)
+                st.write(idx, r.url, r.status_code, r.reason)
 
 
 
