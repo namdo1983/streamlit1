3c9f31ca62a24ae128407e21fcfd8c197aab9e98
NamAtlantech <nam.do@creqavn.net>
2021-12-30 10:34:18 +0700

    snowfalke2

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 mkdir -p ~/.streamlit/
 
+
 echo "\
 [server]\n\
 headless = true\n\
@@ -7,3 +8,12 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
+mkdir -p ~/.style/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.style/style.css
