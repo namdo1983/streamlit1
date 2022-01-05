77dc152f7d4ebbb044e1eafc30990562e5041988
NamAtlantech <nam.do@creqavn.net>
2022-01-05 17:47:46 +0700

    update domain chua active

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 import speedtest
 from datetime import datetime
 from time import perf_counter
+from PIL import Image
+import base64
 
 
 s = requests.Session()
@@ -11,11 +13,6 @@ my_headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKi
 s.headers.update(my_headers)
 
 date_object = datetime.now().strftime("%d_%B_%Y_%H_%M_%S")
-
-#Page Title
-st.set_page_config(
-        page_title="Check Access Domain", layout='wide'
-)
 
 
 def get_network_name():
@@ -60,13 +57,12 @@ def load_snow_animate():
 
 @st.cache(suppress_st_warning=True, show_spinner=False)
 def convert_df(df):
-    return df.to_csv().encode('utf-8')
+    return df.to_csv(index=False).encode('utf-8')
 
 def layout():
     ip, my_isp = get_network_name()
     print(my_isp)
     st.title('Please import a file...')
-    st.text('Content of file should be start with HTTPS or only scheme domain')
     st.text('Example: https://google.com or google.com')
     uploaded_file = st.file_uploader('Select A File with *.TXT', type=['txt'])
     if uploaded_file is None:
@@ -103,9 +99,11 @@ def check_url(data):
         if b'http' not in item:
             item = b'https://' + item
         try:
-            r = s.get(item.strip(), timeout=5)
+            r = s.get(item.strip(), headers=my_headers, timeout=5)
+            r.encoding='utf-8'
+            # print(r.text)
         except Exception as e:
-            print(e)
+            # print(e)
             new_e = str(e)
             new_e = new_e.split(': ')[-1]
             # err = {f'{idx}. {item.decode("utf-8").strip()}': str(e)}
@@ -114,15 +112,67 @@ def check_url(data):
             my_table.append(err)
         else:
             if r.ok:
-                print('OK')
-            else:
-                print(item, r.status_code, r.reason)
+                print('Checking domain expired...')
+                check_domain_results(my_table, idx, r)
+            elif r.status_code == 503 and 'Checking your browser before accessing' in r.text:
+                content = idx, r.url, "DDoS protection by Cloudflare"
+                my_table.append(content)
+            elif r.status_code == 500 and '{"status":false,"msg":"Internal Server Error"}' in r.text:
+                content = idx, r.url, '{"status":false,"msg":"Internal Server Error"}'
+                my_table.append(content)
+            elif r.status_code == 522 and 'Connection timed out' in r.text:
+                content = idx, r.url, 'Error 522'
+                my_table.append(content)
+
             content = idx, r.url, r.reason
             my_table.append(content)
     return my_table
 
+def check_domain_results(my_table, idx, r):
+    if 'This domain has expired. Is this your domain?' in r.text:
+        content = idx, r.url, 'This domain has expired. Is this your domain?'
+        my_table.append(content)
+    elif '404 Page' in r.text:
+        content = idx, r.url, "404 Page"
+        my_table.append(content)
+    elif 'This premium domain has expired' in r.text:
+        content = idx, r.url, "This premium domain has expired"
+        my_table.append(content)
+    elif "AN NINH" in r.text:
+        content = idx, r.url, "BỘ CÔNG AN"
+        my_table.append(content)
+    elif "Why purchase this domain with Epik?" in r.text:
+        content = idx, r.url, "Why purchase this domain with Epik?"
+        my_table.append(content)
+    elif "Registered at Namecheap.com" in r.text:
+        content = idx, r.url, "Registered at Namecheap.com"
+        my_table.append(content)
+    elif "Domain parked by OnlyDomains" in r.text:
+        content = idx, r.url, "Domain parked by OnlyDomains"
+        my_table.append(content)
+    elif "This domain is parked free of charge with NameSilo.com" in r.text:
+        content = idx, r.url, "This domain is parked free of charge with NameSilo.com"
+        my_table.append(content)
+    elif "Get WordPress" in r.text:
+        content = idx, r.url, "Get WordPress"
+        my_table.append(content)
+    elif "mobifone.vn/typing-wrong" in r.text:
+        content = idx, r.url, "404 Góc Trái"
+        my_table.append(content)
+    elif len(r.history) >= 2:
+        r_301 = idx, r.url, f'Access OK => Redirect: {r.url}'
+        my_table.append(r_301)
+
 
 def main():
+    #Page Title
+    st.set_page_config(
+            page_title="Check Access Domain", layout='wide'
+    )
+
+    image = Image.open(r'D:\python\streamlit_python\banner-noel.webp')
+    st.image(image, use_column_width=True)
+    
     try:
         load_snow_animate()
         layout()
